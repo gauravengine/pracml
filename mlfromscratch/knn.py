fe02ef9c4a035bbaf5e15c4a0b459ebd317da97f
gauravengine <gengine232@gmail.com>
2021-12-10 06:17:25 +0530

    knn

--- mlfromscratch/knn.py
+++ mlfromscratch/knn.py
@@ -14,7 +14,7 @@ class KNN:
     def fit(self, X, y):
         self.X_train = X
         self.y_train = y
-
+        
     def predict(self, X):
         y_pred = [self._predict(x) for x in X]
         return np.array(y_pred)
